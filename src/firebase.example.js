a91d3a988ca56be41fa3721c4c6959ef65ed8c70
riffs14 <rauhl_c@students.iitmandi.ac.in>
2025-10-10 23:00:58 +0530

    added firebase template for key

---
+++ src/firebase.example.js
@@ -0,0 +1,31 @@
+// Import the functions you need from Firebase SDK
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth"; // Import Firebase Authentication
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "replace with your key",
+  authDomain: "replace with your key",
+  databaseURL: "replace with your key",
+  projectId: "replace with your key",
+  storageBucket: "lreplace with your key",
+  messagingSenderId: "replace with your key",
+  appId: "1:replace with your key",
+  measurementId: "G-replace with your key"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firestore
+const db = getFirestore(app);
+
+// Initialize Firebase Authentication
+const auth = getAuth(app); // Firebase Authentication instance
+
+// Export Firestore and Authentication instances
+export { db, auth };
+
+
+// Thi
